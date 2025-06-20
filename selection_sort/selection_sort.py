c7d746b008d980efdefdf7b91cb86179d265cab4
stile <brian.ikubu@strathmore.edu>
2025-06-20 10:25:01 +0300

    implement bubble sort and selection sort algorithms

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,12 @@
 def selection_sort(arr):
-    # TODO: Implement selection sort
-    pass
+    n = len(arr)
+    for i in range(n):          
+        min_index = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    
+    return arr 
+
+
